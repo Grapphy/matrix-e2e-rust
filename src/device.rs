a44ed5a99619050c06c20265a38b479b7290ebc7
Grapphy <76534455+Grapphy@users.noreply.github.com>
2023-01-31 18:41:55 -0300

    Generating keys and signatures for share

--- src/device.rs
+++ src/device.rs
@@ -1,5 +1,7 @@
+use crate::crypto::{DeviceKey, OneTimeKey};
 use crate::error::Error;
 use crate::http::HTTPBackend;
+use std::collections::HashMap;
 use vodozemac::olm;
 
 pub struct Device {
@@ -18,13 +20,15 @@ impl Device {
         access_token: String,
         homeserver_uri: String,
     ) -> Self {
+        let mut olm_account = olm::Account::new();
+        olm_account.generate_one_time_keys(50);
         Device {
             user_id: user_id,
             device_id: device_id,
             access_token: access_token.clone(),
             homeserver_uri: homeserver_uri.clone(),
             backend_api: HTTPBackend::new(homeserver_uri, access_token),
-            olm_account: olm::Account::new(),
+            olm_account: olm_account,
         }
     }
 
@@ -50,5 +54,34 @@ impl Device {
 
     pub fn ed25519_key(&self) -> String {
         self.olm_account.ed25519_key().to_base64()
+    }
+
+    pub async fn publish_keypair(&self) -> Result<i16, Error> {
+        let device_key = DeviceKey::new(
+            self.device_id.clone(),
+            self.user_id.clone(),
+            self.curve25519_key(),
+            self.ed25519_key(),
+        )
+        .sign(&self.olm_account);
+
+        let mut one_time_keys: HashMap<String, OneTimeKey> = HashMap::new();
+        for (id, curve_key) in self.olm_account.one_time_keys() {
+            let otk = OneTimeKey::new(id.to_base64(), curve_key.to_base64()).sign(
+                &self.olm_account,
+                self.user_id.clone(),
+                self.device_id.clone(),
+            );
+            one_time_keys.insert(
+                format!("signed_curve25519:{}", &otk.id[otk.id.len() - 6..]),
+                otk,
+            );
+        }
+
+        let response = self
+            .backend_api
+            .send_keys(device_key, one_time_keys)
+            .await?;
+        Ok(response.one_time_key_counts)
     }
 }
