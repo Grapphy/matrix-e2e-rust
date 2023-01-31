f0b6f4ac795c9a8a2062ab80eb4a1c465c3c5f0d
Grapphy <76534455+Grapphy@users.noreply.github.com>
2023-01-31 18:41:16 -0300

    Adding key upload responses

--- src/response.rs
+++ src/response.rs
@@ -12,3 +12,8 @@ pub struct LoginResponse {
     pub device_id: String,
     pub access_token: String,
 }
+
+#[derive(Debug, Deserialize)]
+pub struct KeyUploadResponse {
+    pub one_time_key_counts: i16,
+}
