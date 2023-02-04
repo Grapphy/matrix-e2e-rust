e24b94cb505f50d71967b4ba9e291cff39b542ef
Grapphy <76534455+Grapphy@users.noreply.github.com>
2023-02-04 17:07:23 -0300

    exports

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -5,3 +5,7 @@ pub mod one_time_key;
 pub use one_time_key::OneTimeKey;
 
 pub mod megolm_sha2;
+pub use megolm_sha2::{MegolmMessage, MegolmSession};
+
+pub mod olm_sha256;
+pub use olm_sha256::OlmExchange;
